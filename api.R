056bd384a8de25ec0394feca009de876cde46a37
naroalaria <naroa.laria@alumni.mondragon.edu>
2025-06-04 10:04:28 +0200

    exploracion y limpieza

---
+++ api.R
@@ -0,0 +1,25 @@
+#api
+library(digest)
+library(discretization)
+library(dplyr)
+library(EnvStats)
+library(fastDummies)
+library(naniar)
+library(nortest)
+library(readxl)
+library(smoothmest)
+library(VIM)
+library(plumber)
+datos <- read.csv("C:/Users/naroa/Downloads/netflix.csv")
+apiexamen<-"
+#*@apiTitle api examen reto 4
+#*@apiDescription generar una API con ciertas características
+function(Minutes)
+datosdeminutos <- as.numeric(datos$Minutes)
+datoscontransformacion <- log(datos$Minutes)
+return(hist(datos$Minutes))
+"
+writeLines(apiexamen,"api.R")
+r <- plumb("api.R")
+r$run(port=8000)
+
