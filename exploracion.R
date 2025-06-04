bb633c3cabea7728b70e7ffd764c342f44fee9d1
naroalaria <naroa.laria@alumni.mondragon.edu>
2025-06-04 09:15:48 +0200

    en exploracion

--- exploracion.R
+++ exploracion.R
@@ -19,3 +19,22 @@ desvseas <- sd(datos$Seasons,na.rm=T)
 length(unique(datos$type))
 length(unique(datos$country))
 length(unique(datos$director))
+
+#por variable
+miss_var_summary(datos)
+
+#visualizar
+boxplot(datos$Seasons)
+boxplot(datos$Minutes)
+boxplot(datos$release_year)
+
+#MNAR,MAR,MCAR
+vis_miss(datos)
+
+#imputar los NA
+imputar <- kNN(datos, k=5)
+unique(imputar["Minutes"])
+unique(imputar["Seasons"])
+
+
+
