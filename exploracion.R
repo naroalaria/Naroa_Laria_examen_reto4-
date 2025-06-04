0e88bb1e301a9fffab003668c24f383be6bfb807
naroalaria <naroa.laria@alumni.mondragon.edu>
2025-06-04 08:52:56 +0200

    en exploracion

---
+++ exploracion.R
@@ -0,0 +1,21 @@
+library(digest)
+library(discretization)
+library(dplyr)
+library(EnvStats)
+library(fastDummies)
+library(naniar)
+library(nortest)
+library(readxl)
+library(smoothmest)
+library(VIM)
+library(plumber)
+
+datos<-read.csv("C:\\Users\\naroa\\Downloads\\netflix.csv")
+
+summary(datos)
+desvmin <- sd(datos$Minutes,na.rm=T)
+desvseas <- sd(datos$Seasons,na.rm=T)
+
+length(unique(datos$type))
+length(unique(datos$country))
+length(unique(datos$director))
